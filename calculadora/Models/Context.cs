8270fdd4b7421f8dceaa132152ea0287f444ba74
luizsantos-1 <64617480+luizsantos-1@users.noreply.github.com>
2022-11-21 11:11:45 -0300

    Add packages and classes for operation persistence

---
+++ calculadora/Models/Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace calculadora.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Operation> Operations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Operation>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private static Operation[] GetProducts()
+        {
+            return new Operation[]
+            {
+            new Operation("((1+3)*(1^2))"),
+
+
+            new Operation("((6/2)+1)")
+            };
+        }
+    }
+}
+
