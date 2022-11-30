87dc824b303fbac509db045209f5a677152fd697
Luiz Eduardo Tojal <64617480+luizsantos-1@users.noreply.github.com>
2022-11-30 19:58:02 -0300

    Create database context

--- calculadora/App.xaml.cs
+++ calculadora/App.xaml.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using calculadora.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 using System.Windows;
 
 namespace calculadora
@@ -13,6 +12,26 @@ namespace calculadora
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+
+            services.AddDbContext<Context>(options =>
+            {
+                options.UseSqlite("Data source = Operation.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
 
     }
 }
