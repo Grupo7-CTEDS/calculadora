10b303bc5f6cb3dad55851159416bc9a786f1f93
Luiz Eduardo Tojal <64617480+luizsantos-1@users.noreply.github.com>
2022-11-30 21:06:56 -0300

    Enable saving operations and reading saving operations

--- calculadora/MainWindow.xaml.cs
+++ calculadora/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using calculadora.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,42 @@ namespace calculadora
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly Context context;
+
+        Operation newOperation = new()
         {
+            Id = Guid.NewGuid(),
+            creationTime = DateTime.Now
+        };
+        public MainWindow(Context context)
+        {
+            this.context = context;
             InitializeComponent();
+
+            
+            NewOperationGrid.DataContext = newOperation;
+
+
+        }
+
+        private void ListOperations(object sender, RoutedEventArgs e)
+        {
+            List<Operation> OperationList = context.Operations.ToList();
+            MessageBox.Show(OperationList.Last().op);
+
+        }
+
+        private void SaveOperation(object sender, RoutedEventArgs e)
+        {
+            context.Operations.Add(newOperation);
+            context.SaveChanges();
+
+            newOperation = new Operation()
+            {
+                Id = Guid.NewGuid(),
+                creationTime = DateTime.Now
+            };
+            NewOperationGrid.DataContext = newOperation;
         }
     }
 }
