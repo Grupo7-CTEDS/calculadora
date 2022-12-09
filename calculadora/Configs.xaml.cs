00b71d7f477d6f3c45b99c57e6bb5fbc6a507510
luizsantos-1 <64617480+luizsantos-1@users.noreply.github.com>
2022-12-09 20:51:01 -0300

    Update Configs Window

--- calculadora/Configs.xaml.cs
+++ calculadora/Configs.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace CalculadoraInterface
+namespace calculadora
 {
     /// <summary>
     /// Lógica interna para Configs.xaml
